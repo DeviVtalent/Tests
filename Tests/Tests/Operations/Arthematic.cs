fa8972db27419a3f01700fcffdac405559d1fadc
DeviVtalent <Devi9.vtalent@gmail.com>
2019-12-20 13:38:55 +0530

    added new method in Arthematic class

--- Tests/Tests/Operations/Arthematic.cs
+++ Tests/Tests/Operations/Arthematic.cs
@@ -12,13 +12,23 @@ namespace Tests.Operations
                 int a = 10, b = 20;
                 int c = a+ b;
                 return c;
-           }       
-                
+           }                 
 
         
         public int Subtraction(int a, int b)
         {
             return a - b;
+        }
+        public int Multiplication()
+        {
+            int a = 10;
+            int b = 20;
+            return a * b;
+
+        }
+        public void Div()
+        {
+           
         }
 
     }
