4bc61fdd7ac03d2fec412adf971b5697f19b2995
DeviVtalent <Devi9.vtalent@gmail.com>
2019-12-20 13:19:49 +0530

    New class is added in Operations folder in Test Project

---
+++ Tests/Tests/Operations/Arthematic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tests.Operations
+{
+    public class Arthematic
+    {
+        static void Main(string[] args)
+        {
+            int Addition()
+           {
+                int a = 10, b = 20;
+                int c = a+ b;
+                return c;
+           }
+
+        }
+       
+    }
+}
